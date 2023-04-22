f50d5cae1e34a8d05767a731d4498514b9a6c27c
adrian <acygan51@gmail.cp>
2023-04-22 17:03:14 +0200

    Working app -router -json server

---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { TaskService } from 'src/app/services/task.service';
+import { Task } from 'src/app/Task';
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent {
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) { }
+
+  ngOnInit() {
+    this.taskService.getTasks().subscribe(tasks => this.tasks = tasks)
+  }
+
+  deleteTask(task: Task) {
+    this.taskService
+      .deleteTask(task)
+      .subscribe(
+          () => this.tasks = this.tasks.filter(t => t.id !== task.id)
+      )
+  }
+
+  toggleReminder(task: Task) {
+    task.reminder = !task.reminder;
+    this.taskService
+      .updateTaskReminder(task)
+      .subscribe();
+  }
+
+  addTask(task: Task) {
+    this.taskService
+      .addTask(task)
+      .subscribe(tasnk => this.tasks.push(task))
+  }
+}
